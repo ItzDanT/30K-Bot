59deb8528acd190ce0e15d4e194985a2469d7399
ItzDanT <42703695+ItzDanT@users.noreply.github.com>
2018-09-01 17:34:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-323160008411971585const Discord = require('discord.js');
+const Discord = require('discord.js');
 const db = require('quick.db');
 const client = new Discord.Client();   
 const giphy = require('giphy-api')();    
